6addebc9c1691c58015344525193ce86da577ad2
Charlie Palm <charliepalm@Charlies-MacBook-Air.local>
2025-02-11 17:09:28 -0600

    add routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MyComponentComponent } from './my-component/my-component.component';
+
+const routes: Routes = [
+  { path: '', component: MyComponentComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
