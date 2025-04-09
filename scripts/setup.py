6c0fb704fd8ca3677e0747eebb33e8070c8bdc3d
Charlie Palm <cwpalm@wisc.edu>
2025-04-09 13:22:21 -0500

    main | improve and rename setup script

--- scripts/setup.py
+++ scripts/setup.py
@@ -18,11 +18,11 @@ def update_website_template(name: str):
     cleaned_name = re.sub(r'(?<!^)(?=[A-Z])', ' ', name)
 
     # change if needed
-    base_dir = '.'
+    base_dir = f'../{website_name}'
 
     # paths to the specific files to be updated
     package_json = os.path.join(base_dir, "package.json")
-    package_lock = os.path.join(base_dir, "package_lock.json")
+    package_lock = os.path.join(base_dir, "package-lock.json")
     app_component_ts = os.path.join(base_dir, "src", "app", "app.component.ts")
     index_html = os.path.join(base_dir, "src", "index.html")
     angular_json = os.path.join(base_dir, "angular.json")
@@ -30,19 +30,17 @@ def update_website_template(name: str):
     replace_in_file(package_json, '"name": "template"', f'"name": "{name.lower()}"')
     replace_in_file(app_component_ts, "title = 'Template';", f"title = '{cleaned_name}';")
     replace_in_file(index_html, "<title>Template</title>", f"<title>{cleaned_name}</title>")
-    replace_in_file(package_lock, 'template', cleaned_name)
-    replace_in_file(angular_json, "Template", cleaned_name)
+    replace_in_file(package_lock, '"name": "template"', name.lower())
+    replace_in_file(angular_json, "Template", name)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
       print("Usage: python update_template.py <name>")
       sys.exit(1)
 
-    # Get the website name from the command line arguments
     website_name = sys.argv[1]
-
-    # Run the update function
-    os.system(f'cp -r ../* ../{website_name}')
-    os.system('rm -r -f .git')
-    os.system('git init')
+    def check(result):
+      if result != 0: exit(result)
+    check(os.system(f'cd .. && cp -r TailwindTemplate {website_name}'))
+    check(os.system(f'cd ../{website_name} && rm -r -f .git && git init'))
     update_website_template(website_name)
